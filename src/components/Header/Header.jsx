394f42c06dca3fb0c1a9c8ef6f60752d7ed98963
tusharbachchan <tusharbachchan@gmail.com>
2023-12-08 23:47:54 +0530

    updated Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Logo, LogoutBtn } from "../index";
+import { Logo, LogoutBtn, Container } from "../index";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -11,17 +11,71 @@ import { useSelector } from "react-redux";
  *  3. Logout button depending on the fact if the user is logged in or not
  */
 
-// creating array outside funtional component
-
 const Header = () => {
-  return (
-    <>
-      {/* 1. Logo */}
-      <Logo />
-      <h1>Hi</h1>
-      {/* 2. Navbar and Nav-Items */}
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  // creating array outside funtional component
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
 
-    </>
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        {/* 1. Logo */}
+        {/* 2. Navbar and Nav-Items */}
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) => {
+              return item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null;
+            })}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   );
 };
 
--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -4,8 +4,15 @@ import { logout } from '../../store/authSlice'
 import authService from '../../appwrite/auth'
 
 const LogoutBtn = () => {
+  const dispatch = useDispatch()
+  const logoutHandler = () => {
+    authService.logout()
+    .then(() => {
+      dispatch(logout())
+    })
+  }
   return (
-    <button className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">Logout</button>
+    <button className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full" onClick={logoutHandler}>Logout</button>
   );
 }
 
